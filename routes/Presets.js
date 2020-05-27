2405ca0e6802ed37d4a2e2771b26baed2f28f172
ThomasFoydel <thomasjfoydel@gmail.com>
2020-05-27 18:13:06 -0500

    fixed user mongoose model import 2

--- routes/Presets.js
+++ routes/Presets.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const auth = require('../middlewares/auth');
-const User = require('../models/user');
+const User = require('../models/User');
 
 const router = express.Router();
 
