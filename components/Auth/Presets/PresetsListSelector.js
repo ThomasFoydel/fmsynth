7a734bdd254015837799506b59b60c7920604da6
ThomasFoydel <thomasjfoydel@gmail.com>
2023-01-29 02:02:13 -0500

    preset selector scroll behavior

--- components/Auth/Presets/PresetsListSelector.js
+++ components/Auth/Presets/PresetsListSelector.js
@@ -1,29 +1,43 @@
 import cn from 'classnames'
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import { CTX } from '../../../context/Synth/SynthProvider'
 import styles from './Presets.module.scss'
 
 const PresetsListSelector = ({ closeSaveDelete }) => {
-  const [appState, updateState] = useContext(CTX)
-  const presets = appState.presets
+  const [{ presets, currentPreset }, updateState] = useContext(CTX)
+
+  const containerRef = useRef()
 
   const handleSelection = (e) => {
     const name = e.target.id
-    if (name !== appState.currentPreset?.name) {
-      const payload = appState.presets.find((p) => p.name === name)
+    if (name !== currentPreset?.name) {
+      const payload = presets.find((p) => p.name === name)
       if (payload) updateState({ type: 'LOAD_PRESET', payload })
       closeSaveDelete()
     }
   }
 
+  useEffect(() => {
+    if (containerRef.current && containerRef.current.childNodes) {
+      let selectedNode
+      for (let child of containerRef.current.childNodes) {
+        if (child.textContent === currentPreset.name) {
+          selectedNode = child
+          break
+        }
+      }
+      if (selectedNode) selectedNode.scrollIntoView({ behavior: 'smooth' })
+    }
+  }, [currentPreset])
+
   return (
-    <div className={styles.presetListSelector}>
+    <div ref={containerRef} className={styles.presetListSelector}>
       {presets.map((preset, i) => (
         <div
           key={preset.name}
           className={cn(
             styles.presetOption,
-            preset.name === appState.currentPreset?.name && styles.current,
+            preset.name === currentPreset?.name && styles.current,
             presets.length > 4 && i === presets.length - 1 && styles.lastOption
           )}
           onClick={handleSelection}
